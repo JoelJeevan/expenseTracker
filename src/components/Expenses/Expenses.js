9f900031c8e3dc2581996356f17ba0729a39eb49
JoelJevan <joel.jeevan7@gmail.com>
2021-05-21 21:30:12 +1200

    Dynamic lists with filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,20 +5,25 @@ import Card from '../UI/Card';
 import './Expenses.css'
 
 function Expenses(props) {
-    const [filterValue, setfilterValue] = useState('2020');
+    const [filterYear, setFilterYear] = useState('2020');
+     //const [filterArray, setFilterArray] = useState();
+   
 
-    function onFilterValueSelectHandler(filterValue) {
-        setfilterValue(filterValue)
-        console.log(filterValue)
+    function onFilterValueSelectHandler(selectedFilterValue) {
+        setFilterYear(selectedFilterValue)
     }
+
+    const  initialArray = props.items.filter(year => year.date.getFullYear().toString() === filterYear)
 
     return (
         <div>
             <Card className="expenses">
 
-                <ExpensesFilter selected={filterValue} onFilterValueSelect={onFilterValueSelectHandler}></ExpensesFilter>
-                {props.items.map(itemsx =>
+                <ExpensesFilter selected={filterYear} onFilterValueSelect={onFilterValueSelectHandler}></ExpensesFilter>
+                
+                {initialArray.map(itemsx =>
                     <ExpenseItem
+                        key={itemsx.id}
                         title={itemsx.title}
                         amount={itemsx.amount}
                         date={itemsx.date}
